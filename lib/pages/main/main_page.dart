70ff8707966db79a8bdd3265d900d067f507e948
yymin1022 <yymin1022@gmail.com>
2024-12-19 02:41:57 +0900

    Update main_page : Applied Scroll to Opposite Page when Tap

--- lib/pages/main/main_page.dart
+++ lib/pages/main/main_page.dart
@@ -14,6 +14,7 @@ class MainPage extends StatefulWidget {
 
 class _MainPageState extends State<MainPage> {
   final ScrollController _scrollController = ScrollController();
+  bool isCalendarEnabled = true;
 
   @override
   Widget build(BuildContext context) {
@@ -24,16 +25,26 @@ class _MainPageState extends State<MainPage> {
       ),
       body: Listener(
         onPointerUp: (_) {
-          _handleScrollSnap();
+          _handleScrollSnap(_scrollController.offset);
         },
         child: SingleChildScrollView(
           controller: _scrollController,
           scrollDirection: Axis.horizontal,
-          child: const Row(
+          child: Row(
             children: [
-              CalendarView(),
-              TimeListview(),
-              TodoView()
+              GestureDetector(
+                onTap: () {
+                  if(!isCalendarEnabled) _handleScrollSnap(0);
+                },
+                child: const CalendarView()
+              ),
+              const TimeListview(),
+              GestureDetector(
+                onTap: () {
+                  if(isCalendarEnabled) _handleScrollSnap(_scrollController.position.maxScrollExtent);
+                },
+                child: const TodoView()
+              )
             ],
           ),
         ),
@@ -41,23 +52,27 @@ class _MainPageState extends State<MainPage> {
     );
   }
 
-  void _handleScrollSnap() {
-    var curScrollPosition = _scrollController.offset;
+  void _handleScrollSnap(double curScrollPosition) {
     var maxScrollPosition = _scrollController.position.maxScrollExtent;
     var curScrollRatio = curScrollPosition / maxScrollPosition * 100;
 
-    if(curScrollRatio < 50) {
-      _scrollController.animateTo(
-        0,
-        duration: const Duration(milliseconds: 300),
-        curve: Curves.easeOut,
-      );
-    } else {
-      _scrollController.animateTo(
-        maxScrollPosition,
-        duration: const Duration(milliseconds: 300),
-        curve: Curves.easeOut,
-      );
-    }
+    setState(() {
+      if(curScrollRatio < 50) {
+        isCalendarEnabled = true;
+        _scrollController.animateTo(
+          0,
+          duration: const Duration(milliseconds: 300),
+          curve: Curves.easeOut,
+        );
+      } else {
+        isCalendarEnabled = false;
+        _scrollController.animateTo(
+          maxScrollPosition,
+          duration: const Duration(milliseconds: 300),
+          curve: Curves.easeOut,
+        );
+      }
+    });
+
   }
 }
