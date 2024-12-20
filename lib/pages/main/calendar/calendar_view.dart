ed3ad3d7255736ded5eba8ce4a0156a156668b05
yymin1022 <yymin1022@gmail.com>
2024-12-20 17:06:11 +0900

    Update calendar_view : Added Dummy CalendarItem ListView

--- lib/pages/main/calendar/calendar_view.dart
+++ lib/pages/main/calendar/calendar_view.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:todo_project/pages/main/calendar/calendar_item.dart';
 
 class CalendarView extends StatefulWidget {
   const CalendarView({super.key, required this.isPageEnabled});
@@ -9,12 +10,21 @@ class CalendarView extends StatefulWidget {
 }
 
 class _CalendarViewState extends State<CalendarView> {
+  final colorList = [Colors.blue, Colors.white];
+  final itemCnt = 100;
+
   @override
   Widget build(BuildContext context) {
     return Container(
       height: MediaQuery.of(context).size.height,
       width: MediaQuery.of(context).size.width - 120,
       color: widget.isPageEnabled ? Colors.blue : Colors.white,
+      child: ListView.builder(
+        itemCount: itemCnt,
+        itemBuilder: (_, idx) {
+          return CalendarItem(itemColor: colorList[idx % 2]);
+        }
+      ),
     );
   }
 }
