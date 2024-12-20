a9d41c23dd04357eb77b9c150877421265515c96
yymin1022 <yymin1022@gmail.com>
2024-12-20 17:06:57 +0900

    Update todo_view : Added Dummy TodoItem ListView

--- lib/pages/main/todo/todo_view.dart
+++ lib/pages/main/todo/todo_view.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:todo_project/pages/main/todo/todo_item.dart';
 
 class TodoView extends StatefulWidget {
   const TodoView({super.key, required this.isPageEnabled});
@@ -9,12 +10,21 @@ class TodoView extends StatefulWidget {
 }
 
 class _TodoViewState extends State<TodoView> {
+  final colorList = [Colors.green, Colors.white];
+  final itemCnt = 100;
+
   @override
   Widget build(BuildContext context) {
     return Container(
       height: MediaQuery.of(context).size.height,
       width: MediaQuery.of(context).size.width - 120,
-      color: widget.isPageEnabled ? Colors.green : Colors.white,
+      color: widget.isPageEnabled ? Colors.blue : Colors.white,
+      child: ListView.builder(
+          itemCount: itemCnt,
+          itemBuilder: (_, idx) {
+            return TodoItem(itemColor: colorList[idx % 2]);
+          }
+      ),
     );
   }
 }
