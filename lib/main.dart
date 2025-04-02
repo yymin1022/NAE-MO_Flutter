95c89adeed6ddee581c62edaddd96e09c3ab96e4
Hoon <ldh019@cau.ac.kr>
2025-04-03 00:48:41 +0900

    Update main : Add navigation

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,16 @@
 import "package:flutter/material.dart";
+import "package:flutter_native_splash/flutter_native_splash.dart";
+import "package:todo_project/pages/login/login_page.dart";
 import "package:todo_project/pages/main/main_page.dart";
+import "package:todo_project/pages/setting/setting_page.dart";
 import "package:todo_project/util/firebase_util.dart";
 
 void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
+  WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
+  FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
   await FirebaseUtil().initFirebase();
 
+  FlutterNativeSplash.remove();
   runApp(const MyApp());
 }
 
@@ -20,7 +25,12 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MainPage(title: "Flutter Demo Home Page"),
+      initialRoute: '/main',
+      routes: {
+        '/main': (context) => const MainPage(title: "Flutter Demo Home Page"),
+        '/login': (context) => const LoginPage(title: "Login Page"),
+        '/setting': (context) => const SettingPage(title: "Setting Page"),
+      },
     );
   }
 }
