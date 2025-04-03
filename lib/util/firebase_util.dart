2a5ad50b5aee896fce0c16bf370648b78cddbeba
HOONLAB <ldh019@cau.ac.kr>
2025-04-03 16:25:54 +0900

    Update firebase_util : Add sign-in & -out function

--- lib/util/firebase_util.dart
+++ lib/util/firebase_util.dart
@@ -1,18 +1,20 @@
 import 'dart:ui';
 
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 import 'package:todo_project/firebase_options.dart';
 
 class FirebaseUtil {
-  FirebaseUtil._privateConstructor();
-  static final FirebaseUtil _instance = FirebaseUtil._privateConstructor();
 
   factory FirebaseUtil() {
     return _instance;
   }
+  FirebaseUtil._privateConstructor();
+  static final FirebaseUtil _instance = FirebaseUtil._privateConstructor();
 
   late FirebaseFirestore _firestoreDB;
 
@@ -28,5 +30,29 @@ class FirebaseUtil {
     };
 
     _firestoreDB = FirebaseFirestore.instance;
+  }
+
+  bool isUserLoggedIn() {
+    return FirebaseAuth.instance.currentUser != null;
+  }
+
+  Future<UserCredential> signInWithGoogle() async {
+    var googleUser = await GoogleSignIn().signIn();
+
+    if (googleUser == null) {
+      throw Exception("google user is null");
+    }
+    var googleAuth = await googleUser.authentication;
+    var credential = GoogleAuthProvider.credential(
+      accessToken: googleAuth.accessToken,
+      idToken: googleAuth.idToken,
+    );
+
+    return await FirebaseAuth.instance.signInWithCredential(credential);
+  }
+
+  Future<void> signOut() async {
+    await FirebaseAuth.instance.signOut();
+    await GoogleSignIn().signOut();
   }
 }
