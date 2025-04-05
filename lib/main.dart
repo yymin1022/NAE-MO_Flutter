df1abffc9e61017054b2a012e75b1aa9390d4e81
Hoon <ldh019@cau.ac.kr>
2025-04-06 00:01:50 +0900

    Update main : Add Auth Listener

--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,20 @@
 import "package:flutter/material.dart";
 import "package:flutter_native_splash/flutter_native_splash.dart";
-import "package:provider/provider.dart";
-import "package:todo_project/pages/login/AuthProvider.dart";
+import "package:todo_project/pages/login/auth_state_listener.dart";
 import "package:todo_project/pages/login/login_page.dart";
 import "package:todo_project/pages/main/main_page.dart";
 import "package:todo_project/pages/setting/setting_page.dart";
 import "package:todo_project/util/firebase_util.dart";
 
+final GlobalKey<NavigatorState> navigatorKey = GlobalKey<NavigatorState>();
+
 void main() async {
   var widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
   FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
+
   await FirebaseUtil().initFirebase();
 
-  FlutterNativeSplash.remove();
-  runApp(MultiProvider(
-    providers: [
-      ChangeNotifierProvider(create: (_) => AuthProvider()),
-    ],
-    child: const MyApp(),
-  ));
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
@@ -26,19 +22,26 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    var authProvider = Provider.of<AuthProvider>(context);
-    return MaterialApp(
-      title: "Flutter Demo",
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      initialRoute: authProvider.isLoggedIn ? '/main' : '/login',
-      routes: {
-        '/main': (context) => const MainPage(title: "Flutter Demo Home Page"),
-        '/login': (context) => const LoginPage(title: "Login Page"),
-        '/setting': (context) => const SettingPage(title: "Setting Page"),
-      },
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      FlutterNativeSplash.remove();
+    });
+
+    return AuthStateListener(
+      navigatorKey: navigatorKey,
+      child: MaterialApp(
+        navigatorKey: navigatorKey,
+        title: "Flutter Demo",
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+          useMaterial3: true,
+        ),
+        initialRoute: '/login',
+        routes: {
+          '/main': (context) => const MainPage(title: "Flutter Demo Home Page"),
+          '/login': (context) => const LoginPage(title: "Login Page"),
+          '/setting': (context) => const SettingPage(title: "Setting Page"),
+        },
+      )
     );
   }
 }
