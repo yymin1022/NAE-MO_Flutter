b65bfb958ad99f14b8b9f23b346c6f9c625eee02
Hoon <ldh019@cau.ac.kr>
2025-04-06 00:01:26 +0900

    Added auth_state_listener

---
+++ lib/pages/login/auth_state_listener.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:todo_project/util/firebase_util.dart';
+
+class AuthStateListener extends StatefulWidget {
+
+  const AuthStateListener({
+    super.key,
+    required this.navigatorKey,
+    required this.child,
+  });
+  final GlobalKey<NavigatorState> navigatorKey;
+  final Widget child;
+
+  @override
+  State<StatefulWidget> createState() => _AuthStateListenerState();
+}
+
+class _AuthStateListenerState extends State<AuthStateListener> {
+  @override
+  void initState() {
+    super.initState();
+    FirebaseUtil().listenAuthStateChanges(widget.navigatorKey);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return widget.child;
+  }
+}
