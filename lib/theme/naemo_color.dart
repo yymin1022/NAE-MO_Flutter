746a69c745c49f03d65554a9d856d3e137f24d4f
yymin1022 <yymin1022@gmail.com>
2025-04-10 23:59:44 +0900

    Update naemo_color : Defined Naemo Blue

--- lib/theme/naemo_color.dart
+++ lib/theme/naemo_color.dart
@@ -3,4 +3,6 @@ import 'package:flutter/material.dart';
 class NaemoColor {
   static const Color black = Colors.black;
   static const Color white = Colors.white;
+
+  static const Color naemoBlue = Color(0x002b3a67);
 }
