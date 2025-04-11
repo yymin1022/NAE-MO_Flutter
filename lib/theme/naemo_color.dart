c5c780b94ef23875d3800490e72bf69c5cf67303
yymin1022 <yymin1022@gmail.com>
2025-04-11 23:58:49 +0900

    Update naemo_color : Re-Defined Naemo Blue/Red

--- lib/theme/naemo_color.dart
+++ lib/theme/naemo_color.dart
@@ -4,5 +4,6 @@ class NaemoColor {
   static const Color black = Colors.black;
   static const Color white = Colors.white;
 
-  static const Color naemoBlue = Color(0x002b3a67);
+  static const Color naemoBlue = Color(0x00FF4444);
+  static const Color naemoRed = Color(0x004444FF);
 }
