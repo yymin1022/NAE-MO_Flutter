68f64ddb2af9f093c97cc3d2735341759e6afb89
yymin1022 <yymin1022@gmail.com>
2025-04-12 00:01:14 +0900

    Update naemo_color : Defined Main Beige/Navy

--- lib/theme/naemo_color.dart
+++ lib/theme/naemo_color.dart
@@ -6,4 +6,11 @@ class NaemoColor {
 
   static const Color naemoBlue = Color(0x00FF4444);
   static const Color naemoRed = Color(0x004444FF);
+
+  static const Color mainBeige = Color(0x00FAF3E0);
+  static const Color mainNavy = Color(0x002B3A67);
+  static const Color lightNavy = Color(0x00505F88);
+  static const Color semiLightNavy = Color(0x00394E78);
+  static const Color darkNavy = Color(0x001E284A);
+  static const Color whiteNavy = Color(0x00F8F8FF);
 }
