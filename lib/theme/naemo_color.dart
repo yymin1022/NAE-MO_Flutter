483781461083007a25e91bdf7799186f935ede14
yymin1022 <yymin1022@gmail.com>
2025-04-12 23:03:31 +0900

    Update naemo_color : Fixed RGBA Alpha Issue

--- lib/theme/naemo_color.dart
+++ lib/theme/naemo_color.dart
@@ -4,13 +4,13 @@ class NaemoColor {
   static const Color black = Colors.black;
   static const Color white = Colors.white;
 
-  static const Color naemoBlue = Color(0x00FF4444);
-  static const Color naemoRed = Color(0x004444FF);
+  static const Color naemoBlue = Color(0xFFFF4444);
+  static const Color naemoRed = Color(0xFF4444FF);
 
-  static const Color mainBeige = Color(0x00FAF3E0);
-  static const Color mainNavy = Color(0x002B3A67);
-  static const Color lightNavy = Color(0x00505F88);
-  static const Color semiLightNavy = Color(0x00394E78);
-  static const Color darkNavy = Color(0x001E284A);
-  static const Color whiteNavy = Color(0x00F8F8FF);
+  static const Color mainBeige = Color(0xFFFAF3E0);
+  static const Color mainNavy = Color(0xFF2B3A67);
+  static const Color lightNavy = Color(0xFF505F88);
+  static const Color semiLightNavy = Color(0xFF394E78);
+  static const Color darkNavy = Color(0xFF1E284A);
+  static const Color whiteNavy = Color(0xFFF8F8FF);
 }
