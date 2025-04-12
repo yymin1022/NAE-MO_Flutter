b25d7f2d17e5f9513f8350389c6f7396bc658ec6
yymin1022 <yymin1022@gmail.com>
2025-04-12 23:06:35 +0900

    Update naemo_color : Defined Category Colors

--- lib/theme/naemo_color.dart
+++ lib/theme/naemo_color.dart
@@ -18,4 +18,15 @@ class NaemoColor {
   static const Color semiLightNavy = Color(0xFF394E78);
   static const Color darkNavy = Color(0xFF1E284A);
   static const Color whiteNavy = Color(0xFFF8F8FF);
+
+  static const Color category1 = Color(0xFF7EC8E3);
+  static const Color category2 = Color(0xFFA8E55C);
+  static const Color category3 = Color(0xFFFFA474);
+  static const Color category4 = Color(0xFFFDDD48);
+  static const Color category5 = Color(0xFFCBA4E9);
+  static const Color category6 = Color(0xFFCB9460);
+  static const Color category7 = Color(0xFFFF9EB7);
+  static const Color category8 = Color(0xFF9FE2BF);
+  static const Color category9 = Color(0xFFD4E157);
+  static const Color category10 = Color(0xFF5DADE2);
 }
