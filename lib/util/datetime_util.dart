44e4e7944316ad825a12a6b140aa0d46c8a0d9b5
yymin1022 <yymin1022@gmail.com>
2025-04-17 22:28:28 +0900

    Update datetime_util : Added Datetime util functions

--- lib/util/datetime_util.dart
+++ lib/util/datetime_util.dart
@@ -1,3 +1,34 @@
 class DatetimeUtil {
-  
+  factory DatetimeUtil() { return _instance; }
+  DatetimeUtil._privateConstructor();
+  static final DatetimeUtil _instance = DatetimeUtil._privateConstructor();
+
+  DateTime getToday() {
+    return DateTime.now();
+  }
+
+  int getTodayDay() {
+    return getToday().day;
+  }
+
+  int getTodayMonth() {
+    return getToday().month;
+  }
+
+  int getTodayYear() {
+    return getToday().year;
+  }
+
+  int getCurrentHour() {
+    return getToday().hour;
+  }
+
+  int getCurrentMinute() {
+    return getToday().minute;
+  }
+
+  String getCurrentTimeString() {
+    var today = getToday();
+    return "${today.hour}:${today.minute}";
+  }
 }
