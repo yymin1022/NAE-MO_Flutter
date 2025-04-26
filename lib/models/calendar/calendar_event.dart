032942f18493d5a0dfe82218a86e182a542b2727
yymin1022 <yymin1022@gmail.com>
2025-04-27 01:43:21 +0900

    Update calendar_event : Defined From/To Value for each CalendarEvent

--- lib/models/calendar/calendar_event.dart
+++ lib/models/calendar/calendar_event.dart
@@ -2,16 +2,18 @@ import 'package:googleapis/calendar/v3.dart';
 
 class CalendarEvent {
   factory CalendarEvent.fromEvent(Event event) {
-    var startDate = event.start?.dateTime ?? event.start?.date ?? DateTime.now();
-    return CalendarEvent(title: event.summary ?? 'No Title', start: startDate);
+    var timeFrom = event.start?.dateTime ?? event.start?.date ?? DateTime.now();
+    var timeTo = event.end?.dateTime ?? event.end?.date ?? DateTime.now();
+    return CalendarEvent(title: event.summary ?? 'No Title', from: timeFrom, to: timeTo);
   }
 
   factory CalendarEvent.empty() {
-    return CalendarEvent(title: '', start: DateTime(1));
+    return CalendarEvent(title: '', from: DateTime.now(), to: DateTime.now());
   }
 
-  CalendarEvent({required this.title, required this.start});
+  CalendarEvent({required this.title, required this.from, required this.to});
 
   final String title;
-  final DateTime start;
+  final DateTime from;
+  final DateTime to;
 }
